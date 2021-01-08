3bb1be83fbc5c5916e194c0fca710e768f953c5b
lauramiranda99 <69016867+lauramiranda99@users.noreply.github.com>
2021-01-08 17:55:29 -0500

    Create Plot2.R

---
+++ ExploratoryDataAnalysis/Project 1/Plot2.R
@@ -0,0 +1,27 @@
+# Getting the data
+
+download.file('https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip',
+              destfile = "./Exploratory Analysis/dataset.zip")
+
+unzip(zipfile = "./Exploratory Analysis/dataset.zip")
+
+data <- read.table('household_power_consumption.txt', header = T, stringsAsFactors = F,
+                   na.strings = "?", sep = ';')
+
+# Subsetting
+
+dataset = data[as.character(data$Date) %in% c("1/2/2007", "2/2/2007"),]
+
+# Correcting the date format
+
+dataset$dateTime <- strptime(paste(dataset$Date, dataset$Time), format = '%d/%m/%Y %H:%M:%S')
+
+# Plot
+
+plot(y = dataset$Global_active_power, x = dataset$dateTime, type = "l",
+     ylab = "Global Active Power (kilowatts)", xlab = NA)
+
+# Saving to a file
+
+dev.copy(png, file= "Plot2.png", height=480, width=480)
+dev.off()
